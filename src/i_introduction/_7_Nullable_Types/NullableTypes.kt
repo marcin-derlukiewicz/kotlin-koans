5f04f054b2903679c02428ac39ab850f18758aa7
Marcin Derlukiewicz <marcin@derlukiewicz.pl>
2017-06-06 01:45:22 +0200

    kotlin introduction

--- src/i_introduction/_7_Nullable_Types/NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/NullableTypes.kt
@@ -3,14 +3,14 @@ package i_introduction._7_Nullable_Types
 import util.TODO
 import util.doc7
 
-fun test() {
-    val s: String = "this variable cannot store null references"
-    val q: String? = null
-
-    if (q != null) q.length      // you have to check to dereference
-    val i: Int? = q?.length      // null
-    val j: Int = q?.length ?: 0  // 0
-}
+//fun test() {
+//    val s: String = "this variable cannot store null references"
+//    val q: String? = null
+//
+//    if (q != null) q.length      // you have to check to dereference
+//    val i: Int? = q?.length      // null
+//    val j: Int = q?.length ?: 0  // 0
+//}
 
 fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO(
     """
@@ -25,7 +25,9 @@ fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
 ) {
-    todoTask7(client, message, mailer)
+    if(client?.personalInfo?.email!=null && message != null){
+        mailer.sendMessage(client.personalInfo.email,message)
+    }
 }
 
 class Client (val personalInfo: PersonalInfo?)
